84a14f387ce3bdc5d22b1afb667844a7fb4ecc05
Twarit <wtwarit@gmail.com>
2018-05-25 18:10:00 +0530

    Boiler-plate library linkages

---
+++ Rubeus-core/Source/main.cpp
@@ -0,0 +1,14 @@
+/*
+	This is the entry point for Rubeus-core
+*/
+#include <iostream>
+#include <GL/glew.h>
+
+int main()
+{
+	std::cout << "Hello world"<< std::endl;
+	system("PAUSE");
+	glewInit();
+
+	return 0;
+}
