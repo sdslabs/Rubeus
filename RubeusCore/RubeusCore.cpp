b83502d5de3b6ecb5c3a77e97d7845e02ac8f86b
Twarit <wtwarit@gmail.com>
2018-06-11 17:08:41 +0530

    Add testing for using Modern OpenGL constructs

--- RubeusCore/RubeusCore.cpp
+++ RubeusCore/RubeusCore.cpp
@@ -12,11 +12,40 @@ int main()
 								1280, 720,
 								EWindowParameters::WINDOWED_MODE,
 								EWindowParameters::NON_RESIZABLE_WINDOW);
+<<<<<<< HEAD
+=======
 
-	Matrix4 ortho = Matrix4::orthographic(0.0f, 16.0f, 0.0f, 9.0f, -1.0f, 1.0f);
+	GLfloat vertices[] =
+	{
+		-0.5f, -0.5f,
+		-0.5f, +0.5f,
+		+0.5f, +0.5f,
+		+0.5f, -0.5f
+	};
+
+	unsigned int indices[] =
+	{
+		0, 1, 2,
+		2, 3, 0
+	};
+
+	unsigned int vbo;
+	glGenBuffers(1, &vbo);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	glBufferData(GL_ARRAY_BUFFER, 8 * sizeof(GLfloat), vertices, GL_STATIC_DRAW);
+
+	unsigned int ibo;
+	glGenBuffers(1, &ibo);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(unsigned int), indices, GL_STATIC_DRAW);
+>>>>>>> 584465d... Add testing for using Modern OpenGL constructs
+
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, 0);
 
 	RShaderComponent shader("Shaders/basic.vertexshader", "Shaders/basic.fragmentshader");
 	shader.enableShader();
+<<<<<<< HEAD
 	shader.setUniformMat4("proj_matrix", Matrix4::orthographic(0.0f, 16.0f, 0.0f, 9.0f, -1.0f, 1.0f));
 
 	//GLfloat vertices[] =
@@ -75,10 +104,21 @@ int main()
 
 	RRenderableObject sprite(RML::Vector3D(5, 5, 0), RML::Vector2D(3, 3), RML::Vector4D(1, 0, 1, 1), shader);
 	RSimpleRendererComponent renderer;
+=======
+
+	ERROR("NO");
+	LOG("YES");
+	ASSERT("Maybe");
+>>>>>>> 584465d... Add testing for using Modern OpenGL constructs
 
 	while(!GameWindow.closed())
 	{
 		GameWindow.clearWindow();
+<<<<<<< HEAD
+=======
+
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+>>>>>>> 584465d... Add testing for using Modern OpenGL constructs
 
 		renderer.submit(&sprite);
 		renderer.flush();
