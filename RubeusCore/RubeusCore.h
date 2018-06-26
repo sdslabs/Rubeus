deacf1d6225701d182c4d731765f83fb1813e92d
Twarit <wtwarit@gmail.com>
2018-06-26 22:04:08 +0530

    Fix memory leak checks to debug only

--- RubeusCore/RubeusCore.h
+++ RubeusCore/RubeusCore.h
@@ -7,7 +7,10 @@
 #include <string>
 
 // TODO: Reference additional headers your program requires here.
+#ifdef _DEBUG
 #include <vld.h>
+#endif
+
 #include <GL\glew.h>
 #include <GLFW\glfw3.h>
 #include <rubeus_maths_library.h>
