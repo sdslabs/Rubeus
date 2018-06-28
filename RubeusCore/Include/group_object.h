c781dae91f30fbca323847cf9e3ba6fb67626a66
Twarit <wtwarit@gmail.com>
2018-06-28 14:08:15 +0530

    Add docs to layer related objects

--- RubeusCore/Include/group_object.h
+++ RubeusCore/Include/group_object.h
@@ -13,10 +13,26 @@ namespace Rubeus
 {
 	namespace GraphicComponents
 	{
+		/**
+		 * @class	Group
+		 *
+		 * @brief	A group of objects in a family based hierarchy.
+		 */
 		struct Group
 		{
+			/** @brief	Vector array of renderables. */
 			std::vector<RRenderableObject> renderables;
 
+			/**
+			 * @fn		Group & addRenderable(RRenderableObject * renderable)
+			 *
+			 * @brief	Adds the renderable object as the children of this object
+			 * @warning	Do not pass in dereferenced pointers. This object intends to take ownership of the passed in renderable object.
+			 *
+			 * @param	renderable	The renderable object.
+			 *
+			 * @return	Reference to this group object. Allows chaining addRenderable() calls. E.g. group.addRenderable(r1).addRenderable(r2);
+			 */
 			Group & addRenderable(RRenderableObject * renderable);
 		};
 	}
--- RubeusCore/Include/static_layer_object.h
+++ RubeusCore/Include/static_layer_object.h
@@ -14,24 +14,63 @@
 #include <logger_component.h>
 #include <rubeus_maths_library.h>
 
-// TODO: Add docs
 namespace Rubeus
 {
 	namespace GraphicComponents
 	{
+		/**
+		 * @class	RStaticLayer
+		 *
+		 * @brief	Allows a static layer of groups of objects to be drawn on the screen.
+		 */
 		class RStaticLayer : public RLayer
 		{
 		private:
+			/** @brief	Vector array of all the groups handles by this layer */
 			std::vector<Group *> m_Groups;
+
+			/** @brief	The renderer alloted to this layer */
 			RGuerrillaRendererComponent * m_Renderer;
+
+			/** @brief	The shader for this layer to use */
 			RShaderComponent & m_Shader;
 
 		public:
-
+			/**
+			 * @fn		RStaticLayer(RShaderComponent& shader)
+			 *
+			 * @brief	Constructor. Initialises the layer's shader.
+			 * @warning	Do not pass in derenferences pointers
+			 *
+			 * @param	shader	The shader to allot to this layer.
+			 */
 			RStaticLayer(RShaderComponent& shader);
+
+			/**
+			 * @fn	virtual ~RStaticLayer()
+			 *
+			 * @brief	Destructor
+			 */
 			virtual ~RStaticLayer();
 
+			/**
+			 * @fn		RLayer & addGroup(Group & group) override
+			 *
+			 * @brief	Adds the group object to this layer.
+			 * @warning
+			 *
+			 * @param	group	The group object to add.
+			 *
+			 * @return	Reference to this layer. Allows chaining multiple group additions. E.g. layer.addGoup(g1).addGroup(g2);
+			 */
 			RLayer & addGroup(Group & group) override;
+
+			/**
+			 * @fn		void draw() override
+			 *
+			 * @brief	Draws the layer
+			 * @warning	Call only once per frame
+			 */
 			void draw() override;
 
 		protected:
