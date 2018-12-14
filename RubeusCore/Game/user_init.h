188462a7998fa8771a901a537f5606678c44dc9d
Twarit <wtwarit@gmail.com>
2018-12-14 20:29:13 +0530

    Remove sample game

--- RubeusCore/Game/user_init.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#include "../Game/game_name/levels/engine_files/sample_level.h"
-#include "../Game/game_name/levels/engine_files/sample_level2.h"
-#include "../Game/game_name/levels/engine_files/sample_object.h"
