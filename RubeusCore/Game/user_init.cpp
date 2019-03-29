adf2ba677d7a6af6bb099f6aefd9bc0276f1317d
unknown <nupur202000@gmail.com>
2019-03-30 04:54:23 +0530

    Position vec dimension fixed

--- RubeusCore/Game/user_init.cpp
+++ RubeusCore/Game/user_init.cpp
@@ -19,8 +19,8 @@ protected:
 								0.5f, 3.0f,
 								"Assets/test9.png", true,
 								Rubeus::Awerere::EColliderType::BOX,
-								new Rubeus::Awerere::ABoxCollider(RML::Vector3D(0.0f, 0.0f, 1.0f),
-																  RML::Vector3D(0.5f, 3.0f, 1.0f)),
+								new Rubeus::Awerere::ABoxCollider(RML::Vector2D(0.0f, 0.0f),
+																  RML::Vector2D(0.5f, 3.0f)),
 								true);
 
 		rightPaddle = new paddle("right_paddle", "play_level",
@@ -29,8 +29,8 @@ protected:
 								 "Assets/test8.png",
 								 true,
 								 Rubeus::Awerere::EColliderType::BOX,
-								 new Rubeus::Awerere::ABoxCollider(RML::Vector3D(15.5f, 0.0f, 1.0f),
-																   RML::Vector3D(16.0f, 3.0f, 1.0f)),
+								 new Rubeus::Awerere::ABoxCollider(RML::Vector2D(15.5f, 0.0f),
+																   RML::Vector2D(16.0f, 3.0f)),
 								 true);
 
 		matchBall = new ball("ball",
@@ -40,8 +40,8 @@ protected:
 							 "Assets/test8.png",
 							 true,
 							 Rubeus::Awerere::EColliderType::BOX,
-							 new Rubeus::Awerere::ABoxCollider(RML::Vector3D(8.0f, 4.5f, 1.0f),
-															   RML::Vector3D(8.5f, 5.0f, 1.0f)),
+							 new Rubeus::Awerere::ABoxCollider(RML::Vector2D(8.0f, 4.5f),
+															   RML::Vector2D(8.5f, 5.0f)),
 							 true);
 
 	}
