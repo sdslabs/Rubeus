b33dac96b3a603e1c3ec1c1a7908e666ac88daa7
sin3point14 <manaschaudhary2000@gmail.com>
2019-05-17 19:53:30 +0530

    ImGui sample window implemented

--- RubeusCore/Source/engine.cpp
+++ RubeusCore/Source/engine.cpp
@@ -8,6 +8,9 @@
 
 #include <any>
 
+#include <ImGui/imgui.h>
+#include <ImGui/imgui_impl_glfw.h>
+#include <ImGui/imgui_impl_opengl3.h>
 namespace Rubeus
 {
 	std::unordered_map<int, bool> RInputManager::KeyMap;
@@ -111,6 +114,14 @@ namespace Rubeus
 
         float lastFrameEndTime = (float)glfwGetTime();
 
+		ImGui::CreateContext();
+		ImGui_ImplGlfw_InitForOpenGL(m_Window->m_Window, true);
+		ImGui_ImplOpenGL3_Init("#version 130");
+		ImGui::StyleColorsDark();
+		bool show_demo_window = true;
+		bool show_another_window = false;
+		ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+
 		// Main game loop
 		while (m_Window->closed() == false)
 		{
@@ -123,6 +134,10 @@ namespace Rubeus
 			// Clear window buffer
 			m_Window->clearWindow();
 
+			ImGui_ImplOpenGL3_NewFrame();
+			ImGui_ImplGlfw_NewFrame();
+			ImGui::NewFrame();
+
 			// Tick the entire world once
 			getWorld()->tick();
 
@@ -131,6 +146,45 @@ namespace Rubeus
 
 			// Draw objects
 			m_GameScene->draw();
+
+			if (show_demo_window)
+				ImGui::ShowDemoWindow(&show_demo_window);
+
+			// 2. Show a simple window that we create ourselves. We use a Begin/End pair to created a named window.
+			{
+				static float f = 0.0f;
+				static int counter = 0;
+
+				ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
+
+				ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
+				ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our window open/close state
+				ImGui::Checkbox("Another Window", &show_another_window);
+
+				ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
+				ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
+
+				if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
+					counter++;
+				ImGui::SameLine();
+				ImGui::Text("counter = %d", counter);
+
+				ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+				ImGui::End();
+			}
+
+			// 3. Show another simple window.
+			if (show_another_window)
+			{
+				ImGui::Begin("Another Window", &show_another_window);   // Pass a pointer to our bool variable (the window will have a closing button that will clear the bool when clicked)
+				ImGui::Text("Hello from another window!");
+				if (ImGui::Button("Close Me"))
+					show_another_window = false;
+				ImGui::End();
+			}
+
+			ImGui::Render();
+			ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 
 			// Switch windows draw and display buffers
 			m_Window->updateWindow();
@@ -151,6 +205,9 @@ namespace Rubeus
 				item->m_IsSubmitted = false;
 			}
 		}
+		//ImGui_ImplOpenGL3_Shutdown();
+		ImGui_ImplGlfw_Shutdown();
+		ImGui::DestroyContext();
 
 		m_StartupLevelName = "";
 	}
